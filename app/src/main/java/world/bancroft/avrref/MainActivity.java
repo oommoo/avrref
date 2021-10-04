c45d3343872f5ae2f99533f2b8d88e6a591ad9d3
Vic Bancroft <vic.bancroft@gmail.com>
2021-10-04 13:13:29 -0400

    Initial version with blank activity

---
+++ app/src/main/java/world/bancroft/avrref/MainActivity.java
@@ -0,0 +1,14 @@
+package world.bancroft.avrref;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
