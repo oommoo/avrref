722763b02af0eb9eab1dc16d61b65408b0d50390
Vic Bancroft <vic.bancroft@gmail.com>
2021-10-15 14:59:14 -0400

    implement second activity for login

--- app/src/main/java/world/bancroft/avrref/MainActivity.java
+++ app/src/main/java/world/bancroft/avrref/MainActivity.java
@@ -2,13 +2,46 @@ package world.bancroft.avrref;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import world.bancroft.avrref.ui.login.LoginActivity;
+import world.bancroft.avrref.ui.login.LoginActivityKt;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "Who Dat";
+    private String name = "unknown";
+    private Button who = null;
+    private Button done = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, name);
+
+        who = (Button) findViewById(R.id.button_who);
+        who.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                setContentView(R.layout.activity_login);
+                startActivity(intent);
+            }
+        });
+
+        done = (Button) findViewById(R.id.button_done);
+        done.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                android.os.Process.killProcess(android.os.Process.myPid());
+                System.exit(1);
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/world/bancroft/avrref/ui/login/LoginFormState.kt
@@ -0,0 +1,8 @@
+package world.bancroft.avrref.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState(val usernameError: Int? = null,
+                          val passwordError: Int? = null,
+                          val isDataValid: Boolean = false)
